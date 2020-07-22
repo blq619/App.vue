92a70ae49c755f93118dd127b06d671c8b730ade
kuknam <blq619@naver.com>
2020-07-22 19:33:06 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app"> 
+    <Topbar v-bind:msg="msg" />
+    {{msg}} <br> My name is {{name}}
+    <br />
+    <br />
+    <input type="text" v-model="name" />
+    <label>제목</label>
+    <input type="text" v-model="msg" />
+    <Bottom />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Topbar from './components/Topbar';
+import Bottom from './components/Bottom';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Topbar,
+    Bottom
+    // HelloWorld
+  },
+  data(){
+    return {
+      msg: "Hello Vue JS",
+      name: "KukNam"
+    };
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+ margin:0;
+ padding:0;
 }
+.header {
+  padding: 1rem 0;
+  background: salmon;
+  color: #fff;
+  text-align: center;
+}
+.footer {
+  width: 100%;
+  padding: 1rem 0;
+  position: fixed;
+  text-align: center;
+  bottom: 0;
+  background: #333;
+  color: #fff;
+}
+
 </style>
